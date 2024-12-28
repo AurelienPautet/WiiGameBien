e44f72faa8ce61ae07cfc52c05e035924df8eb94
Aurelien <aureplusyt@gmail.com>
2024-12-28 10:54:36 +0100

    sepcifiv socket module

--- Public/client.js
+++ Public/client.js
@@ -1,5 +1,6 @@
 //
-const socket = io("https://wiitank-2aacc4abc5cb.herokuapp.com/");
+const socket = io("https://wiitank.pautet.net");
+//const socket = io("https://wiitank-2aacc4abc5cb.herokuapp.com/");
 //const socket = io("http://localhost:7000/");
 
 socket.on("welcome", (data) => {});
