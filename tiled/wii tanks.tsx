c0f99a0f079fc85203526d066dadcc4b0330665b
Aurelien <aureplusyt@gmail.com>
2025-06-06 21:36:22 +0200

    clean up

--- tiled/wii tanks.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="wii game" tilewidth="50" tileheight="50" spacing="1" tilecount="81" columns="9">
- <image source="tile set.png" width="500" height="500"/>
-</tileset>
